035f656f77f7704e1c9d02b65e5e2f3e5f67706b
gtfarng <5735512002@psu.ac.th>
2019-02-19 22:40:01 +0700

    upadte

---
+++ workshop/Example/index.js
@@ -0,0 +1,14 @@
+var express = require('express'); 
+var app = express();
+ 
+app.use(express.static('public'));
+app.get('/',(req,res) => {res.send("OK") })
+  
+
+app.set('views', './views') 
+app.set('view engine', 'ejs') 
+app.get('/computer', function(req, res){ res.render('computer', {computers: ['Windows', 'OSX','Android','IOS']}) })
+
+ 
+app.listen(8000);
+console.log('Server is ready!');
